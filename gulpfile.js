1ea681ffe54901037f6c8d718338a9817e4b3bfc
Ryan Leckey <leckey.ryan@gmail.com>
2015-06-24 01:02:17 -0700

    Update release process

--- gulpfile.js
+++ gulpfile.js
@@ -8,3 +8,36 @@ gulp.task("build", function() {
     .pipe($.babel())
     .pipe(gulp.dest("lib"))
 })
+
+function release(importance) {
+  return new Promise(function(resolve) {
+    // Select package.json
+    gulp.src(["package.json"])
+
+    // Bump version on the package.json
+    .pipe($.bump({type: importance}))
+    .pipe(gulp.dest('./'))
+
+    // Commit the changes
+    .pipe($.git.commit("Bump version"))
+
+    // Tag our version
+    .pipe($.tagVersion())
+
+    .on("end", function() {
+      $.git.push("origin", "master", {args: "--follow-tags"}, function() {
+        gulp.src('')
+          .pipe($.shell([
+            "npm publish"
+          ]))
+          .on("end", function() {
+            resolve()
+          })
+      })
+    })
+  })
+}
+
+gulp.task("release:minor", _.partial(release, "minor"));
+gulp.task("release:major", _.partial(release, "major"));
+gulp.task("release:patch", _.partial(release, "patch"));
