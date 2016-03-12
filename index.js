a73d3f0ed501f58bbdb1ae3948ef4b7f20a5d292
Simon Kjellberg <simon.kjellberg@gmail.com>
2016-03-12 14:15:17 +0100

    Implement proper support for beta releases (#4)

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 #!/usr/bin/env node
-require("./lib/main")();
+var argv = require("yargs")
+  .usage("Usage: $0 [options]")
+  .boolean("b")
+  .alias("b", "beta")
+  .describe("b", "Get beta releases")
+  .help("h")
+  .alias("h", "help")
+  .argv;
+
+if (! argv.h) {
+  require("./lib/main")(argv);
+}
